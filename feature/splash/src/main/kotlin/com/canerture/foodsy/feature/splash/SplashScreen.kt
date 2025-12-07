0a914811d2b4790a69739fb16d2aa9e1fa43ae75
candroid <cnrture@gmail.com>
2025-12-07 20:24:06 +0300

    Refactor SplashScreen to use FSIcon, FSSpacer, and FSText composables for improved UI layout

--- feature/splash/src/main/kotlin/com/canerture/foodsy/feature/splash/SplashScreen.kt
+++ feature/splash/src/main/kotlin/com/canerture/foodsy/feature/splash/SplashScreen.kt
@@ -1,16 +1,22 @@
 package com.canerture.foodsy.feature.splash
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.canerture.foodsy.feature.splash.SplashContract.UiEffect
+import com.canerture.ui.components.FSIcon
+import com.canerture.ui.components.FSSpacer
+import com.canerture.ui.components.FSText
 import com.canerture.ui.extensions.collectWithLifecycle
 import com.canerture.ui.theme.FSTheme
 import kotlinx.coroutines.flow.Flow
@@ -33,17 +39,31 @@ fun SplashScreen(
 internal fun SplashContent(
     modifier: Modifier = Modifier,
 ) {
-    Box(
+    Column(
         modifier = modifier,
-        contentAlignment = Alignment.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
     ) {
-        Icon(
-            imageVector = FSTheme.icons.logo,
-            contentDescription = "Foodsy Logo",
+        FSIcon(
+            modifier = Modifier.size(170.dp),
+            vector = FSTheme.icons.logo,
         )
-        Text(
-            text = "Splash Content",
-            fontSize = 24.sp,
+        FSSpacer(12)
+        FSText(
+            text = "Foodsy",
+            style = TextStyle(
+                fontSize = 48.sp,
+                fontWeight = FontWeight.Medium,
+            )
+        )
+        FSSpacer(4)
+        FSText(
+            text = "Food Delivery App",
+            style = TextStyle(
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+            ),
+            color = FSTheme.colors.orange,
         )
     }
 }
