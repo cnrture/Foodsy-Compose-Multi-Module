d8d3c019af647dba6ade88463c400cd4174ab229
candroid <cnrture@gmail.com>
2025-12-08 19:07:32 +0300

    Remove unnecessary SDK and build configuration settings from build.gradle.kts

--- feature/splash/build.gradle.kts
+++ feature/splash/build.gradle.kts
@@ -5,21 +5,6 @@ plugins {
 
 android {
     namespace = "com.canerture.foodsy.feature.splash"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 36
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    buildFeatures {
-        buildConfig = true
-    }
 }
 
 dependencies {
