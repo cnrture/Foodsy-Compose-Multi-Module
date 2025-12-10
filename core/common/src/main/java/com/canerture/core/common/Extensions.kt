170429fbfa0b51a8622ea87ff3aee6b3ab7e3daf
candroid <cnrture@gmail.com>
2025-12-11 01:24:43 +0300

    Add extension functions: implement Boolean?.orFalse() and Result<T>.toUnit() for improved null handling and result conversion

--- core/common/src/main/java/com/canerture/core/common/Extensions.kt
+++ core/common/src/main/java/com/canerture/core/common/Extensions.kt
@@ -5,3 +5,10 @@ fun Int?.orZero() = this ?: 0
 fun Double?.orZero() = this ?: 0.0
 
 fun Boolean?.orFalse() = this ?: false
+
+fun <T> Result<T>.toUnit(): Result<Unit> {
+    return this.fold(
+        onSuccess = { Result.success(Unit) },
+        onFailure = { Result.failure(it) },
+    )
+}
